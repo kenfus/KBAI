0cb82bb56679f36948268edcf0fc76f6681e385f
Vincenzo <vincenzo.timmel@fhnw.ch>
2026-06-03 19:47:34 +0200

    ADds basicsolution for ttt.

--- Kbai_Gaming_v2.1.2/GameAgent.py
+++ Kbai_Gaming_v2.1.2/GameAgent.py
@@ -3,60 +3,68 @@ from typing import Tuple
 from Game import Game, Type
 from Token import Token
 
+TTT_LINES = [
+    [(0,0),(0,1),(0,2)],
+    [(1,0),(1,1),(1,2)],
+    [(2,0),(2,1),(2,2)],
+    [(0,0),(1,0),(2,0)],
+    [(0,1),(1,1),(2,1)],
+    [(0,2),(1,2),(2,2)],
+    [(0,0),(1,1),(2,2)],
+    [(0,2),(1,1),(2,0)],
+]
+
+TTT_CORNERS = [(0,0),(0,2),(2,0),(2,2)]
+
 
 class GameAgent:
     def __init__(self, token: Token):
-        """
-        Initial call to create your agent.
-        This only gets called 1 time and then your agent will play multiple games.
-        """
         self._token = token
 
     def token(self):
         return self._token
 
     def make_move(self, game: Game) -> Tuple[int, int] | int:
-        """
-        This is the main driver of the agent. The game controller will call this with an updated game object
-        every time the agent is expected to make a move.
+        if game.get_type() == Type.TIC_TAC_TOE:
+            return self._ttt_move(game)
+        else:
+            return -1 
 
-        The agent will return a Tuple(int, int) for Tic-tac-toe
-        or just an int for all Connect Four games.
+    def _ttt_move(self, game: Game):
+        board = game.get_board()
+        opp = game.player2_token() if game.player1_token() == self._token else game.player1_token()
 
-        Tic-tac-toe:
-        Return a tuple in row, column form. (row, col)
-        Returning (-1,-1) will make a random selection of available positions.
+        #Win if possible
+        move = self._ttt_find_threat(board, self._token)
+        if move:
+            return move
 
-        Both Row and Column begin at index 0.
-        A typical Tic-Tac-Toe board is laid out such that 0,0 is upper left corner.
+        #Block opponent winning move
+        move = self._ttt_find_threat(board, opp)
+        if move:
+            return move
 
-        Example Board:
-        0,0  0,1  0,2
-        1,0  1,1  1,2
-        2,0  2,1  2,2
+        #  Board is empty start with a corner
+        if (board == '').all():
+            return TTT_CORNERS[0]
 
+        # Take center if free
+        if board[1][1] == '':
+            return (1, 1)
 
-        CONNECT FOUR:
-        Return an integer representing the column to drop the token into (no row is needed only the column)
-        Returning -1 will make a random selection of available positions.
+        #Take a free corner
+        for r, c in TTT_CORNERS:
+            if board[r][c] == '':
+                return (r, c)
 
-        The Column begins at index 0.
-        A typical Connect Four board is laid out such that columns 0 starts on the left side of the play field.
-        As new pieces are added, they will start filling columns from the lowest position in the column.
+        return (-1, -1)
 
-        Example Board
-           | 0 | 1 | 2 | 3 | 4 | 5 | 6 |
-
-           |   |   |   |   |   |   |   |
-           |   |   |   |   |   |   |   |
-           |   |   |   |   |   |   |   |
-           |   |   |   |   |   |   |   |
-           |   |   |   | Y | Y |   |   |
-           | R |   | Y | R | R |   |   |
-
-
-        Remember each game type requires a different output '(int, int)' or 'int'
-
-        Write your code starting here.
-        """
-        return (-1, -1) if game.get_type() == Type.TIC_TAC_TOE else -1
+    def _ttt_find_threat(self, board, token):
+        """Returns the empty cell that completes two-in-a-row for token, or None."""
+        val = token.value()
+        for line in TTT_LINES:
+            values = [board[r][c] for r, c in line]
+            if values.count(val) == 2 and values.count('') == 1:
+                empty_idx = values.index('')
+                return line[empty_idx]
+        return None
