e14ae982470614876f4054c570103274ddf097da
Vincenzo <vincenzo.timmel@fhnw.ch>
2026-06-03 19:57:14 +0200

    Refactor GameAgent methods for clarity and consistency

--- Kbai_Gaming_v2.1.2/GameAgent.py
+++ Kbai_Gaming_v2.1.2/GameAgent.py
@@ -26,41 +26,40 @@ class GameAgent:
 
     def make_move(self, game: Game) -> Tuple[int, int] | int:
         if game.get_type() == Type.TIC_TAC_TOE:
-            return self._ttt_move(game)
+            return self.ttt_move(game)
         else:
             return -1 
 
-    def _ttt_move(self, game: Game):
+    def ttt_move(self, game: Game):
         board = game.get_board()
-        opp = game.player2_token() if game.player1_token() == self._token else game.player1_token()
+        token_opp = game.player2_token() if game.player1_token() == self._token else game.player1_token()
 
         #Win if possible
-        move = self._ttt_find_threat(board, self._token)
+        move = GameAgent._find_if_winning_move_possible(board, self._token)
         if move:
             return move
 
         #Block opponent winning move
-        move = self._ttt_find_threat(board, opp)
+        move = GameAgent._find_if_winning_move_possible(board, token_opp)
         if move:
             return move
 
-        #  Board is empty start with a corner
+        # Board is empty start with a corner
         if (board == '').all():
             return TTT_CORNERS[0]
 
-        # Take center if free
+        # Take center if free 
         if board[1][1] == '':
             return (1, 1)
 
-        #Take a free corner
+        #Take randomly a free corner. Well, half random.
         for r, c in TTT_CORNERS:
             if board[r][c] == '':
                 return (r, c)
 
         return (-1, -1)
 
-    def _ttt_find_threat(self, board, token):
-        """Returns the empty cell that completes two-in-a-row for token, or None."""
+    def _find_if_winning_move_possible(board, token):
         val = token.value()
         for line in TTT_LINES:
             values = [board[r][c] for r, c in line]
